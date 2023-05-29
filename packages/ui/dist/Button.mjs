470885bf585d8ef61eeed7edb83d093c06df4247
coji <coji@techtalk.jp>
2023-05-29 20:18:23 +0900

    remove intermediate files

--- packages/ui/dist/Button.mjs
+++
@@ -1,7 +0,0 @@
-import {
-  Button
-} from "./chunk-F254UTNT.mjs";
-import "./chunk-WR5B5ROH.mjs";
-export {
-  Button
-};
--- packages/ui/dist/Header.mjs
+++
@@ -1,7 +0,0 @@
-import {
-  Header
-} from "./chunk-FT2FSFKZ.mjs";
-import "./chunk-WR5B5ROH.mjs";
-export {
-  Header
-};
